46c8b947230653d5a29eaa39ea7426d3a9a24d55
monsdar <monsdar@8d055be5-607f-0a5b-7668-6dd92fdc3587>
2013-01-27 19:40:49 +0000

    Added: Creating and removing of tasks

--- web/MysqlTaskStorage.php
+++ web/MysqlTaskStorage.php
@@ -14,13 +14,45 @@ class MysqlTaskStorage implements ITaskStorage
     }
     
     public function createTask($text)
-    {
-        print("Creating task with " . $text);
+    {       
+        //get a valid primary key for the new task (higher than the current maximum key)
+        $maxId = 0;
+        $maxIdQuery = "SELECT MAX(TaskId) FROM tasks;";
+        $maxIdResult = mysql_query($maxIdQuery);
+        if( $maxIdResult && mysql_num_rows($maxIdResult) != 0 )
+        {
+            $maxId = (int)mysql_result($maxIdResult, 0, 0);
+            $maxId++; //increase the id for our new task
+        }
+     
+        $newTask = new Task($maxId);
+        $newTask->setText($text);
+        
+        //add the task to the database
+        //
+        //TODO: Prevent SQL injection
+        $addTaskQuery = "INSERT INTO tasks VALUES (%d, '%s');";
+        $addTaskQuery = sprintf($addTaskQuery, $newTask->getIndex(), $newTask->getText());
+        mysql_query($addTaskQuery);
+        
+        //return it, so we can work with it
+        return $newTask;
     }
 
     public function deleteTask($task)
     {
-        print("Removing task #" . $task->id);
+        //simply delete, if there is no such task, it won't harm...
+        //so no checking needed
+        //
+        //TODO: Prevent SQL injection
+        $removeTaskQuery = "DELETE FROM tasks WHERE TaskId=%d;";
+        $removeTaskQuery = sprintf($removeTaskQuery, $task->getIndex());
+        mysql_query($removeTaskQuery);
+        
+        //delete all the activations belonging to the given task
+        $removeActQuery = "DELETE FROM activations WHERE TaskId=%d;";
+        $removeActQuery = sprintf($removeActQuery, $task->getIndex());
+        mysql_query($removeActQuery);        
     }
 
     public function readTasks()
