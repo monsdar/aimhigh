e380f38fc9bace73b9e8144aa56b6675e9112351
monsdar@gmail.com <monsdar@gmail.com@8d055be5-607f-0a5b-7668-6dd92fdc3587>
2013-03-09 15:42:28 +0000

    Added: Presets to database

--- trunk/database/createDatabase.sql
+++ trunk/database/createDatabase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 08. Mrz 2013 um 19:30
+-- Erstellungszeit: 09. Mrz 2013 um 16:41
 -- Server Version: 5.1.66-0+squeeze1
 -- PHP-Version: 5.3.3-7+squeeze14
 
@@ -32,7 +32,44 @@ CREATE TABLE IF NOT EXISTS `activations` (
   `ActivationDate` date NOT NULL,
   PRIMARY KEY (`ActivationId`),
   KEY `fk_activations_tasks_idx` (`TaskId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1680 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1728 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `taskpresets`
+--
+
+CREATE TABLE IF NOT EXISTS `taskpresets` (
+  `PresetId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Title` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `Text` varchar(160) COLLATE utf8_unicode_ci NOT NULL,
+  `Category` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `IsNegative` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`PresetId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=17 ;
+
+--
+-- Daten für Tabelle `taskpresets`
+--
+
+INSERT INTO `taskpresets` (`PresetId`, `Title`, `Text`, `Category`, `IsNegative`) VALUES
+(1, 'Build your Body', 'Exercise often, get fitter, faster and stronger', 'Health', 0),
+(2, 'Bicycler', 'Instead of hitting the road with your car, you drove by bike!', 'Health', 0),
+(3, 'Early Bird', 'Get out of bed early', 'Health', 0),
+(4, 'Elevator', 'You took the elevator when you could take the stairs instead', 'Health', 1),
+(5, 'Cook a meal', 'You created Haute Cuisine just by yourself!', 'Food', 0),
+(6, 'Eat fruit', 'One apple a day keeps the doctor away', 'Food', 0),
+(7, 'Eat vegetables', 'Eat a portion of raw vegetables', 'Food', 0),
+(8, 'Sweets', 'Eating sweets will not help your diet plans', 'Food', 1),
+(9, 'Fast Food', 'Better cook something healthy instead', 'Food', 1),
+(10, 'Do it yourself', 'Spend some time on your personal projects', 'Productivity', 0),
+(11, 'Blog Post', 'Contribute to your blog', 'Productivity', 0),
+(12, 'Do the dishes', 'It''s easier if you do it more often', 'Productivity', 0),
+(13, 'You''re an Expert', 'Commit something to a community like Stackoverflow', 'Productivity', 0),
+(14, 'Ping a friend', 'Contact someone who you haven''t talked to in a while', 'Social', 0),
+(15, 'Explain something', 'Explain something to someone. Anything counts.', 'Social', 0),
+(16, 'Procrastination', 'You got better things to do than 9Gag and Facebook', 'Productivity', 1);
 
 -- --------------------------------------------------------
 
@@ -49,7 +86,20 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `IsNegative` tinyint(1) NOT NULL DEFAULT '0',
   `Offdays` set('MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`TaskId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11379 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12151 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `taskvotes`
+--
+
+CREATE TABLE IF NOT EXISTS `taskvotes` (
+  `VoteId` int(11) NOT NULL AUTO_INCREMENT,
+  `PresetId` int(11) NOT NULL,
+  `KeyId` int(11) NOT NULL,
+  PRIMARY KEY (`VoteId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -62,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `userkeys` (
   `UserKey` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`KeyId`),
   UNIQUE KEY `UserKey` (`UserKey`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1089 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1158 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
