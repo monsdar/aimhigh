49504ededbfcbd5f46e814b14d8777d9093e082c
monsdar@gmail.com <monsdar@gmail.com@8d055be5-607f-0a5b-7668-6dd92fdc3587>
2013-03-13 21:16:59 +0000

    Changed: Now just using one callback instead of multiple ones (not really needed, just bloating the code)

--- trunk/web2/script/interface.js
+++ trunk/web2/script/interface.js
@@ -12,24 +12,22 @@ $.extend({
     //Touches the user, gets the userkey via getUserKey
     //After the data is received and the user is new, it will call the callbacks
     //  callback(isNewUser);
-    touchUser: function(callbacks) {
+    touchUser: function(callback) {
         //call the user, it will be created if not already existing
         var postVars = {userkey: $.getUserkey(), request: 'touchUser'};
         $.post($.getInterfaceUrl(), postVars, function(data) {
             console.log("Touched the user, received the following response: " + data);
             var answer = $.parseJSON(data);
             
-            //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback(answer);
-            });
+            //call the callback
+            callback(answer);
         });
     },
     
     //Queries and returns the tasks of the given user
     //After the data is received it will call the given callback via#
     //  callback(tasks);
-    queryTasks: function(callbacks) {
+    queryTasks: function(callback) {
         var tasks;
         var postVars = {userkey: $.getUserkey(), request: 'getTasks'};
         $.post($.getInterfaceUrl(), postVars, function(data) {
@@ -37,72 +35,62 @@ $.extend({
             console.log("queryTasks() received the following tasks:");
             console.log(tasks);
             
-            //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback(tasks);
-            });
+            //call the callback
+            callback(tasks);
         });
     },
     
     //Toggles a given task
     //After the data is received it will call the given callback via
     //  callback();
-    toggleTask: function(taskId, selectedDate, callbacks) {
+    toggleTask: function(taskId, selectedDate, callback) {
         var postVars = {userkey: $.getUserkey(), request: 'toggleTask', taskid: taskId, date: selectedDate};
         $.post($.getInterfaceUrl(), postVars, function(data) {
             console.log("Toggled task #" + taskId + ", received the following response: " + data);
             
-            //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback();
-            });
+            //call the callback
+            callback();
         });
     },
 
     //deletes a task by its taskId
     //After the data is received it will call the given callback via
     //  callback();
-    deleteTask: function(taskId, callbacks) {
+    deleteTask: function(taskId, callback) {
         var postVars = {userkey: $.getUserkey(), request: 'removeTask', taskid: taskId};
         $.post($.getInterfaceUrl(), postVars, function(data) {
             console.log("Removed task #" + taskId + ", received the following response: " + data);
             
-            //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback();
-            });
+            //call the callback
+            callback();
         });
     },
     
     //edits a given task
     //After the data is received it will call the given callback via
     //  callback();
-    editTask: function(taskId, title, text, category, isNegative, offdays, callbacks) {
+    editTask: function(taskId, title, text, category, isNegative, offdays, callback) {
         //send the interface that the task should be created
         var postVars = {userkey: $.getUserkey(), request: 'updateTask', taskid: taskId, title: title, text: text, category: category, isnegative: isNegative, offdays: offdays};
         $.post($.getInterfaceUrl(), postVars, function(data) {
             console.log("Edited task " + taskId + ", received the following response: " + data);
             
-            //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback();
-            });
+            //call the callback
+            callback();
         });
     },
     
     //creates a task
     //After the data is received it will call the given callback via
     //  callback();
-    createTask: function(title, text, category, isNegative, offdays, callbacks) {
+    createTask: function(title, text, category, isNegative, offdays, callback) {
         //send the interface that the task should be created
         var postVars = {userkey: $.getUserkey(), request: 'createTask', title: title, text: text, category: category, isnegative: isNegative, offdays: offdays};
         $.post($.getInterfaceUrl(), postVars, function(data) {
             console.log("Created new task, received the following response: " + data);
             
             //call the callbacks
-            $.each( callbacks, function(i, callback) {
-                callback();
-            });
+            callback();
         });
     }    
 });
